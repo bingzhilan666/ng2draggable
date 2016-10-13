4a8d858c7008fd2febd66d7f90565ee5c7ed462a
Ced c <cedvandenbosch@gmail.com>
2016-10-13 18:50:26 +0200

    mobile fix

--- draggable.directive.ts
+++ draggable.directive.ts
@@ -21,25 +21,52 @@ export class Draggable implements OnInit{
       }
     }
 
-    @HostListener('mousedown', ['$event'])
-    onMouseDown(event:MouseEvent) {
-      this.md = true;
-      this.topStart = event.clientY - this.element.nativeElement.style.top.replace('px','');
-      this.leftStart = event.clientX - this.element.nativeElement.style.left.replace('px','');
-    }
 
-    @HostListener('document:mouseup')
-    onMouseUp() {
-      this.md = false;
-    }
+        @HostListener('mousedown', ['$event'])
+        onMouseDown(event:MouseEvent) {
+          console.log("mouse down");
+          this.md = true;
+          this.topStart = event.clientY - this.element.nativeElement.style.top.replace('px','');
+          this.leftStart = event.clientX - this.element.nativeElement.style.left.replace('px','');
+        }
 
-    @HostListener('document:mousemove', ['$event'])
-    onMouseMove(event:MouseEvent) {
-      if(this.md && this._allowDrag){
-        this.element.nativeElement.style.top = (event.clientY - this.topStart) + 'px';
-        this.element.nativeElement.style.left = (event.clientX - this.leftStart) + 'px';
-      }
-    }
+        @HostListener('document:mouseup')
+        onMouseUp() {
+          this.md = false;
+        }
+
+        @HostListener('mousemove', ['$event'])
+        onMouseMove(event:MouseEvent) {
+          console.log("mouse move");
+          if(this.md && this._allowDrag){
+            this.element.nativeElement.style.top = (event.clientY - this.topStart) + 'px';
+            this.element.nativeElement.style.left = (event.clientX - this.leftStart) + 'px';
+          }
+        }
+
+        @HostListener('touchstart', ['$event'])
+        onTouchStart(event:TouchEvent) {
+          console.log("touch start");
+          this.md = true;
+          this.topStart = event.changedTouches[0].clientY - this.element.nativeElement.style.top.replace('px','');
+          this.leftStart = event.changedTouches[0].clientX - this.element.nativeElement.style.left.replace('px','');
+          event.stopPropagation();
+        }
+
+        @HostListener('document:touchend')
+        onTouchEnd() {
+          this.md = false;
+        }
+
+        @HostListener('touchmove', ['$event'])
+        onTouchMove(event:TouchEvent) {
+          console.log("touch move");
+          if(this.md && this._allowDrag){
+            this.element.nativeElement.style.top = (event.changedTouches[0].clientY - this.topStart) + 'px';
+            this.element.nativeElement.style.left = (event.changedTouches[0].clientX - this.leftStart) + 'px';
+          }
+          event.stopPropagation();
+        }
 
     @Input('ng2-draggable')
     set allowDrag(value:boolean){
